776469fd02af6c9b082f88c7544abf839b6ec6ec
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-03-25 00:09:19 -0300

    aplicando rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from'@angular/material/sidenav'
-import { MatListModule } from'@angular/material/list'
+import { MatCardModule } from'@angular/material/card'
+import { MatListModule } from'@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutoComponent } from './views/produto/produto.component'
+
 
 @NgModule({
   declarations: [
@@ -19,6 +23,8 @@ import { MatListModule } from'@angular/material/list'
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
+    ProdutoComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,8 @@ import { MatListModule } from'@angular/material/list'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
