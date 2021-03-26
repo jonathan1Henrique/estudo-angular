7ca0ece447a689645b695b177079f830540f9cc3
Jonathan Henrique <jonathan1_henrique@hotmail.com>
2021-03-25 22:37:52 -0300

    component criar produto e modelo produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProdutoCreateComponent } from './components/produto/produto-create/produto-create.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProdutoComponent } from './views/produto/produto.component';
 
@@ -11,10 +12,14 @@ const routes: Routes = [
     path:"",
     component: HomeComponent
   },
-{
-  path:"produto",
-  component: ProdutoComponent
-}
+  {
+    path:"produto",
+    component: ProdutoComponent
+  },
+  {
+    path:"produto/create",
+    component: ProdutoCreateComponent
+  }
 ];
 
 @NgModule({
---
+++ src/app/components/produto/produto-create/produto-create.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-create',
+  templateUrl: './produto-create.component.html',
+  styleUrls: ['./produto-create.component.css']
+})
+export class ProdutoCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
